eb49bf8e43b32ea971c25405de95080c5bc561ec
Jon Linton <jon@studio-424.com>
2017-03-25 22:31:58 -0500

    projects page, updated nav, express view for projects

--- server.js
+++ server.js
@@ -38,6 +38,12 @@ app.get('/about', (req, res) => {
 	})
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'projects'
+	})
+});
+
 app.get('/', (req, res) => {
 	res.render('home.hbs', {
 		pageTitle: 'Home',
